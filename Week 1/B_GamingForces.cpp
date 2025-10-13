db0bc31f63be130abcfc606ed0aa29e5863c6d47
Rafa <ramijahsultana@gmail.com>
2025-10-13 22:34:37 +0600

    1

--- Week 1/B_GamingForces.cpp
+++ Week 1/B_GamingForces.cpp
@@ -5,6 +5,41 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int n;
+        cin >> n;
+        vector<int> a(n);
+        for(int i=0; i<n; i++) cin >> a[i];
+        
+        sort(a.begin(), a.end());
+        int ans = 0;
+        bool flag = false;
+        for(int i=0; i<n; i++)
+        {
+            if(a[i] == a[i+1])
+            {
+                a[i] = a[i] -1;
+                a[i+1] = a[i+1] -1;
+                ans++;
+                if(a[i] < 0) a[i] = 0;
+                if(a[i+1] < 0) a[i+1] = 0;
+                flag = true;
+
+            }
+            else
+            {
+                cout << n << "\n";
+                break;
+            }
+        }
+
+        if(flag) cout << n - ans << "\n";
+
+
+    }
     
 
     return 0;
