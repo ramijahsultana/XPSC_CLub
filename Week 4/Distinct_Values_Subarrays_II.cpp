3048b77f356c21c88e8704a5a1a53ac9f1436219
Rafa <ramijahsultana@gmail.com>
2025-12-29 08:31:46 +0600

    week 4

---
+++ Week 4/Distinct_Values_Subarrays_II.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    
+
+    return 0;
+}
---
+++ Week 4/Subarray_Sums_II.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n, ex_sum;
+    cin >> n >> ex_sum;
+
+    map<ll, ll> sum_freq;
+    int sum = 0;
+
+    return 0;
+}
