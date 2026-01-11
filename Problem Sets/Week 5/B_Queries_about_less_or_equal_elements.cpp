95d66aae69b18ff8a696f27f496a02b16b1243a7
Rafa <ramijahsultana@gmail.com>
2026-01-12 02:43:40 +0600

    -Week 5

---
+++ Problem Sets/Week 5/B_Queries_about_less_or_equal_elements.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+#define ll long long
+#define pb push_back
+
+template<typename T> using pbds = tree<T, null_type, less_equal<T>, rb_tree_tag, 
+tree_order_statistics_node_update>;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n, m;
+    cin >> n >> m;
+    pbds<int> s;
+    for(int i=1; i<=n; i++)
+    {
+        int x;
+        cin >> x;
+        s.insert(x);
+    }
+
+    for(int i=1; i<=m; i++)
+    {
+        int x;
+        cin >> x;
+        cout << s.order_of_key(x+1) << " ";
+    }
+    cout << "\n";
+    
+
+
+
+    return 0;
+}
