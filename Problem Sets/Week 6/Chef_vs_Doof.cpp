47015ebd5f5b50862a2b9c1c69ce1cbe752394d1
Rafa <ramijahsultana@gmail.com>
2026-03-18 06:43:56 +0600

    GCD_LCM math

---
+++ Problem Sets/Week 6/Chef_vs_Doof.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int n;
+        cin >> n;
+        int even = false;
+        for(int i=0; i<n; i++){
+            int x;
+            cin >> x;
+            if(x%2 == 0)
+            {
+                even = true;
+            }
+        }
+
+        if(even == false)
+            cout << "YES\n";
+        else
+            cout << "NO\n";    
+    }
+
+    return 0;
+}
